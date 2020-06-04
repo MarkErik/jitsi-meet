f36fae54292d5ae5d52be6ca2d5ce283c0ba4f29
Mark <altosaar@gmail.com>
2020-06-04 07:46:19 -0400

    Update WelcomePage.web.js
    Hide the room name panel

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -193,7 +193,7 @@ class WelcomePage extends AbstractWelcomePage {
                                 { app: APP_NAME }) }
                         </p>
                     </div>
-                    <div id = 'enter_room'>
+                    {/* <div id = 'enter_room'>
                         <div className = 'enter-room-input-container'>
                             <div className = 'enter-room-title'>
                                 { t('welcomepage.enterRoomTitle') }
@@ -223,7 +223,7 @@ class WelcomePage extends AbstractWelcomePage {
                                     : t('welcomepage.go')
                             }
                         </div>
-                    </div>
+                    </div> */}
                     { this._renderTabs() }
                 </div>
                 { showAdditionalContent
