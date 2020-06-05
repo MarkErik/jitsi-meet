f6ff654c01726ac772a4f7bf153384bcdbc033d8
Mark <altosaar@gmail.com>
2020-06-04 20:20:02 -0400

    Update RemoteVideoMenuTriggerButton.js

--- react/features/remote-video-menu/components/web/RemoteVideoMenuTriggerButton.js
+++ react/features/remote-video-menu/components/web/RemoteVideoMenuTriggerButton.js
@@ -232,14 +232,14 @@ class RemoteVideoMenuTriggerButton extends Component<Props> {
         //         participantID = { participantID } />
         // );
 
-        if (onVolumeChange) {
-            buttons.push(
-                <VolumeSlider
-                    initialValue = { initialVolumeValue }
-                    key = 'volume-slider'
-                    onChange = { onVolumeChange } />
-            );
-        }
+        // if (onVolumeChange) {
+        //     buttons.push(
+        //         <VolumeSlider
+        //             initialValue = { initialVolumeValue }
+        //             key = 'volume-slider'
+        //             onChange = { onVolumeChange } />
+        //     );
+        // }
 
         if (buttons.length > 0) {
             return (
