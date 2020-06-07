24fc67228e80177c765710d116a799c460c7800e
Mark <altosaar@gmail.com>
2020-06-07 15:20:58 -0400

    Only show small thumbnails of the remote videos when there are 3 or more participants

--- react/features/filmstrip/functions.web.js
+++ react/features/filmstrip/functions.web.js
@@ -47,19 +47,20 @@ export function shouldRemoteVideosBeVisible(state: Object) {
     return Boolean(
         participantCount > 2
 
-            // Always show the filmstrip when there is another participant to
-            // show and the filmstrip is hovered, or local video is pinned, or
-            // the toolbar is displayed.
-            || (participantCount > 1
-                && (state['features/filmstrip'].hovered
-                    || state['features/toolbox'].visible
-                    || ((pinnedParticipant = getPinnedParticipant(state))
-                        && pinnedParticipant.local)))
+        // Always show the filmstrip when there is another participant to
+        // show and the filmstrip is hovered, or local video is pinned, or
+        // the toolbar is displayed.
+        // || (participantCount > 1
+        //     && (state['features/filmstrip'].hovered
+        //         || state['features/toolbox'].visible
+        //         || ((pinnedParticipant = getPinnedParticipant(state))
+        //             && pinnedParticipant.local)))
 
-            || (typeof interfaceConfig === 'object'
-                && interfaceConfig.filmStripOnly)
+        || (typeof interfaceConfig === 'object'
+            && interfaceConfig.filmStripOnly)
 
-            || state['features/base/config'].disable1On1Mode);
+        //|| state['features/base/config'].disable1On1Mode
+    );
 }
 
 /**
